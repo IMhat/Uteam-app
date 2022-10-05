ee366d5e60f2e7aa5089da91e71db6c486e1c6e4
Matiaslarragnetta22 <matiaslarragnetta22@gmail.com>
2022-10-05 14:57:28 -0300

    tab bar

--- flutter-front/lib/screens/manage_task_screens.dart
+++ flutter-front/lib/screens/manage_task_screens.dart
@@ -1,4 +1,5 @@
 import 'package:app_uteam/providers/task_form_provider.dart';
+import 'package:app_uteam/screens/backlog_screen.dart';
 
 import 'package:app_uteam/services/task_services.dart';
 import 'package:app_uteam/widgets/task_card.dart';
@@ -44,129 +45,166 @@ class _ManageTaskScreenBody extends StatefulWidget {
   State<_ManageTaskScreenBody> createState() => _ManageTaskScreenBodyState();
 }
 
-class _ManageTaskScreenBodyState extends State<_ManageTaskScreenBody> {
+class _ManageTaskScreenBodyState extends State<_ManageTaskScreenBody>
+    with SingleTickerProviderStateMixin {
+  int selectedPage = 0;
+  TabController? controller;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    controller =
+        TabController(length: 3, initialIndex: selectedPage, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    controller!.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
       backgroundColor: Colors.white,
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                const SizedBox(height: 20),
-                const Text(
-                  "Mis desafíos",
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700),
-                ),
-                Container(
-                  margin: const EdgeInsets.only(top: 30),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      Container(
-                        padding: const EdgeInsets.only(
-                          top: 10,
-                        ),
-                        width: 100,
-                        height: 45,
-                        decoration: BoxDecoration(
-                            color: Colors.green,
-                            border: Border.all(
-                                color:
-                                    const Color.fromARGB(255, 255, 251, 251)),
-                            borderRadius: BorderRadius.circular(50)),
-                        child: const Text(
-                          "All",
-                          style: TextStyle(
-                              fontSize: 15,
-                              fontWeight: FontWeight.bold,
-                              color: Color.fromARGB(255, 0, 0, 0)),
-                          textAlign: TextAlign.center,
-                        ),
-                      ),
-                      Container(
-                        padding: const EdgeInsets.only(
-                          top: 10,
-                        ),
-                        width: 120,
-                        height: 45,
-                        decoration: BoxDecoration(
-                            color: const Color.fromARGB(255, 255, 98, 0),
-                            border: Border.all(
-                                color:
-                                    const Color.fromARGB(255, 255, 251, 251)),
-                            borderRadius: BorderRadius.circular(50)),
-                        child: const Text(
-                          "En progreso",
-                          style: TextStyle(
-                              fontSize: 15,
-                              fontWeight: FontWeight.bold,
-                              color: Color.fromARGB(255, 0, 0, 0)),
-                          textAlign: TextAlign.center,
-                        ),
-                      ),
-                      Container(
-                        padding: const EdgeInsets.only(
-                          top: 10,
-                        ),
-                        width: 120,
-                        height: 45,
-                        decoration: BoxDecoration(
-                            color: Colors.green,
-                            border: Border.all(
-                                color:
-                                    const Color.fromARGB(255, 255, 251, 251)),
-                            borderRadius: BorderRadius.circular(50)),
-                        child: const Text(
-                          "Finalizado",
-                          style: TextStyle(
-                              fontSize: 15,
-                              fontWeight: FontWeight.bold,
-                              color: Color.fromARGB(255, 0, 0, 0)),
-                          textAlign: TextAlign.center,
-                        ),
-                      ),
-                    ],
-                  ),
-                )
-              ],
-            ),
-            Container(
-              margin: const EdgeInsets.only(top: 30, left: 20, bottom: 20),
-              width: 350,
+      body: Column(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+                border: Border(
+                    bottom: BorderSide(width: 1, color: Colors.deepPurple))),
+            child: Material(
               color: Colors.white,
-              child: Center(
-                child: ListView.builder(
-                  shrinkWrap: true,
-                  physics: const NeverScrollableScrollPhysics(),
-                  scrollDirection: Axis.vertical,
-                  //itemCount: taskListProvider.tasks.length,
-                  itemCount: widget.tasksService.tasks.length,
-
-                  itemBuilder: (BuildContext context, int index) =>
-                      GestureDetector(
-                    onTap: () {
-                      widget.tasksService.selectedTask =
-                          widget.tasksService.tasks[index].copy();
-                      Navigator.pushNamed(
-                        context,
-                        'AceptTasks',
-                      );
-                    },
-                    child: TaskCard(
-                      task: widget.tasksService.tasks[index],
+              child: TabBar(
+                controller: controller,
+                labelColor: Colors.black,
+                unselectedLabelColor: Colors.grey,
+                tabs: [
+                  Tab(
+                    child: Container(
+                      child: Text("Backlog"),
                     ),
                   ),
-                ),
+                  Tab(
+                    child: Container(
+                      child: Text("In Progress"),
+                    ),
+                  ),
+                  Tab(
+                    child: Container(
+                      child: Text("Done"),
+                    ),
+                  ),
+                ],
               ),
             ),
-          ],
-        ),
+          ),
+          Expanded(
+              child: TabBarView(
+            controller: controller,
+            children: [
+              Text("data"),
+              Text("data2"),
+              Text("data3"),
+            ],
+          ))
+        ],
       ),
+      // Container(
+      //   margin: const EdgeInsets.only(top: 30, left: 20, bottom: 20),
+      //   width: 350,
+      //   color: Colors.white,
+      //   child: Center(
+      //     child: ListView.builder(
+      //       shrinkWrap: true,
+      //       physics: const NeverScrollableScrollPhysics(),
+      //       scrollDirection: Axis.vertical,
+      //       //itemCount: taskListProvider.tasks.length,
+      //       itemCount: widget.tasksService.tasks.length,
+
+      //       itemBuilder: (BuildContext context, int index) =>
+      //           GestureDetector(
+      //         onTap: () {
+      //           widget.tasksService.selectedTask =
+      //               widget.tasksService.tasks[index].copy();
+      //           Navigator.pushNamed(
+      //             context,
+      //             'AceptTasks',
+      //           );
+      //         },
+      //         child: TaskCard(
+      //           task: widget.tasksService.tasks[index],
+      //         ),
+      //       ),
+      //     ),
+      //   ),
+      // ),
     );
   }
 }
+
+// Container(
+//   padding: const EdgeInsets.only(
+//     top: 10,
+//   ),
+//   width: 100,
+//   height: 45,
+//   decoration: BoxDecoration(
+//       color: Colors.green,
+//       border: Border.all(
+//           color:
+//               const Color.fromARGB(255, 255, 251, 251)),
+//       borderRadius: BorderRadius.circular(50)),
+//   child: const Text(
+//     "All",
+//     style: TextStyle(
+//         fontSize: 15,
+//         fontWeight: FontWeight.bold,
+//         color: Color.fromARGB(255, 0, 0, 0)),
+//     textAlign: TextAlign.center,
+//   ),
+// ),
+// Container(
+//   padding: const EdgeInsets.only(
+//     top: 10,
+//   ),
+//   width: 120,
+//   height: 45,
+//   decoration: BoxDecoration(
+//       color: const Color.fromARGB(255, 255, 98, 0),
+//       border: Border.all(
+//           color:
+//               const Color.fromARGB(255, 255, 251, 251)),
+//       borderRadius: BorderRadius.circular(50)),
+//   child: const Text(
+//     "En progreso",
+//     style: TextStyle(
+//         fontSize: 15,
+//         fontWeight: FontWeight.bold,
+//         color: Color.fromARGB(255, 0, 0, 0)),
+//     textAlign: TextAlign.center,
+//   ),
+// ),
+// Container(
+//   padding: const EdgeInsets.only(
+//     top: 10,
+//   ),
+//   width: 120,
+//   height: 45,
+//   decoration: BoxDecoration(
+//       color: Colors.green,
+//       border: Border.all(
+//           color:
+//               const Color.fromARGB(255, 255, 251, 251)),
+//       borderRadius: BorderRadius.circular(50)),
+//   child: const Text(
+//     "Finalizado",
+//     style: TextStyle(
+//         fontSize: 15,
+//         fontWeight: FontWeight.bold,
+//         color: Color.fromARGB(255, 0, 0, 0)),
+//     textAlign: TextAlign.center,
+//   ),
+// ),
